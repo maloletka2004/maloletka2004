2015f804916ade2ec44533bfb5d8422fb1057a09
maloletka2004 <104685009+maloletka2004@users.noreply.github.com>
2022-04-30 17:31:35 +0300

    Add files via upload
    v1.0 beta

---
+++ shooter_game.py
@@ -0,0 +1,188 @@
+#Создай собственный Шутер!
+
+#from pygame import *
+
+#mixer.music.play()
+#fire_sound = mixer.Sound('fite.ogg')
+
+#img_back = 'back.jpg'
+#img_hero = "smoke.png"
+
+#class GameSprite(sprite.Sprite):
+   # def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        #sprite.Sprite.__init__(self)
+
+        #self.image = transform.scale(image.load(player_image))
+        #self.speed = player.speed
+        #self.rect = self.image.get.rect()
+        #self.rect.x = player_x
+        #self.rect.y = player_y
+
+    #def reset(self):
+        #window.bilit(self.image, (self.rect.x, self.rect.y))
+    
+
+    #class Player(GameSprite):
+        #def update(self):
+           # keys = key.get.
+
+
+
+
+from pygame import *
+from randint import*
+from time import*
+#фоновая музыка
+mixer.init()
+mixer.music.load('space.ogg')
+mixer.music.play()
+fire_sound = mixer.Sound('fire.ogg')
+ 
+font.init()
+ 
+ #нам нужны такие картинки:
+img_back = "galaxy.jpg" #фон игры
+img_hero = "rocket.png" #герой
+ 
+#класс-родитель для других спрайтов
+class GameSprite(sprite.Sprite):
+ #конструктор класса
+   def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+       #Вызываем конструктор класса (Sprite):
+       sprite.Sprite.__init__(self)
+ 
+       #каждый спрайт должен хранить свойство image - изображение
+       self.image = transform.scale(image.load(player_image), (size_x, size_y))
+       self.speed = player_speed
+ 
+       #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+       self.rect = self.image.get_rect()
+       self.rect.x = player_x
+       self.rect.y = player_y
+ #метод, отрисовывающий героя на окне
+   def reset(self):
+       window.blit(self.image, (self.rect.x, self.rect.y))
+class Player(GameSprite):
+   #метод для управления спрайтом стрелками клавиатуры
+   def update(self):
+       keys = key.get_pressed()
+       if keys[K_LEFT] and self.rect.x > 5:
+           self.rect.x -= self.speed
+       if keys[K_RIGHT] and self.rect.x < win_width - 80:
+           self.rect.x += self.speed
+ #метод "выстрел" (используем место игрока, чтобы создать там пулю)
+   def fire(self):
+       pass
+ 
+#Создаем окошко
+win_width = 700
+win_height = 500
+display.set_caption("Shooter")
+window = display.set_mode((win_width, win_height))
+background = transform.scale(image.load(img_back), (win_width, win_height))
+ 
+#создаем спрайты
+ship = Player(img_hero, 5, win_height - 100, 80, 100, 10)
+ 
+#переменная "игра закончилась": как только там True, в основном цикле перестают работать спрайты
+finish = False
+#Основной цикл игры:
+run = True #флаг сбрасывается кнопкой закрытия окна
+while run:
+   #событие нажатия на кнопку Закрыть
+    for e in event.get():
+        if e.type == QUIT:
+           run = F
+if not finish:
+       #обновляем фон
+        window.blit(background,(0,0))
+ 
+       #производим движения спрайтов
+        ship.update()
+ 
+       #обновляем их в новом местоположении при каждой итерации цикла
+        ship.reset()
+ 
+        display.update()
+   #цикл срабатывает каждые 0.05 секунд
+        time.delay(3000)
+        for i in range(1, 6):
+            monster = Enemy(img_enemy, randint(80, win_width - 80) -40, 80, 50, randint(1, 5))
+            monster.add(monsters)
+
+
+   time.delay(50)
+
+
+
+
+def los(self):
+        if self.rect.y <= 0:
+            self.rect.y = 5
+            self.rect.x = randint(1, 495), 5
+
+
+
+
+
+
+
+
+
+display.update()
+mixer.init()
+mixed.music.load('mtheme.mp3')
+
+
+font = font.SysFont('Arial',36)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
